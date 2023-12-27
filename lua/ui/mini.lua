747c15d249460bd81965228b0d79f93d236a0db6
namelessenko <namelessenko@gmail.com>
2023-12-27 21:37:11 +0100

    mini.indentsocpe fixed for filetypes

--- lua/ui/mini.lua
+++ lua/ui/mini.lua
@@ -1,1 +1,28 @@
-require('mini.indentscope').setup()
+require("mini.indentscope").setup({
+	symbol = "│",
+	options = { try_as_border = true },
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	desc = "Disable indentscope for certain filetypes",
+	callback = function()
+		local ignore_filetypes = {
+			"aerial",
+			"dashboard",
+			"help",
+			"lazy",
+			"leetcode.nvim",
+			"mason",
+			"neo-tree",
+			"NvimTree",
+			"neogitstatus",
+			"notify",
+			"startify",
+			"toggleterm",
+			"Trouble",
+		}
+		if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+			vim.b.miniindentscope_disable = true
+		end
+	end,
+})

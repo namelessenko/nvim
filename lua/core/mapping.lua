95768ab360abefde1328c6bc26088e28622fa757
namelessenko <namelessenko@gmail.com>
2023-12-29 23:56:05 +0100

    mappings*

--- lua/core/mapping.lua
+++ lua/core/mapping.lua
@@ -1,5 +1,55 @@
 vim.g.mapleader = "\\"
 
+vim.keymap.set("n", "x", '"_x')
+
+-- Increment/decrement
+vim.keymap.set("n", "+", "<C-a>")
+vim.keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwards
+vim.keymap.set("n", "dw", 'vb"_d')
+
+-- Select all
+vim.keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Save with root permission (not working for now)
+--vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
+
+-- Disable continuations
+vim.keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
+vim.keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
+
+-- Jumplist
+vim.keymap.set("n", "<C-m>", "<C-i>", opts)
+
+-- New tab
+vim.keymap.set("n", "te", ":tabedit")
+vim.keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+vim.keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+-- Split window
+vim.keymap.set("n", "ss", ":split<Return>", opts)
+vim.keymap.set("n", "sv", ":vsplit<Return>", opts)
+-- Move window
+vim.keymap.set("n", "sh", "<C-w>h")
+vim.keymap.set("n", "sk", "<C-w>k")
+vim.keymap.set("n", "sj", "<C-w>j")
+vim.keymap.set("n", "sl", "<C-w>l")
+
+-- Resize window
+vim.keymap.set("n", "<C-w><left>", "<C-w><")
+vim.keymap.set("n", "<C-w><right>", "<C-w>>")
+vim.keymap.set("n", "<C-w><up>", "<C-w>+")
+vim.keymap.set("n", "<C-w><down>", "<C-w>-")
+
+-- Diagnostics
+vim.keymap.set("n", "<C-j>", function()
+	vim.diagnostic.goto_next()
+end, opts)
+
+vim.keymap.set("n", "<leader>r", function()
+	require("craftzdog.utils").replaceHexWithHSL()
+end)
+
 --Neotree
 vim.keymap.set("n", "<leader>e", ":Neotree left toggle<CR>")
 vim.keymap.set("n", "<leader>o", ":Neotree float git_status<CR>")
